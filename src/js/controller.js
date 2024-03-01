20ce2507e110c54c304d74b934f1d1c41f263589
gsikaras <sykarasioannis@hotmail.com>
2024-03-01 15:42:26 +0200

    WE BACK!.. Helpers N confi files!

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,14 +8,6 @@ import 'regenerator-runtime/runtime'; //polyphilling Async Await
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -43,4 +35,3 @@ const controlRecipes = async function () {
 ); //same shit prettier
 // window.addEventListener('hashchange', controlRecipes);
 // window.addEventListener('load', controlRecipes);
-console.log('testing pame ligo');
