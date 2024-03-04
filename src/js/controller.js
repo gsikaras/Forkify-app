eaebb0a404197a8cab1246d316ffc2c458cdf009
gsikaras <sykarasioannis@hotmail.com>
2024-03-04 17:54:12 +0200

    implementing Search results N views! pff..

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,11 +2,9 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
-
+import searchView from './views/searchView.js';
 import 'core-js/stable'; // Polyplhilling everything else
 import 'regenerator-runtime/runtime'; //polyphilling Async Await
-
-const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -15,7 +13,6 @@ const recipeContainer = document.querySelector('.recipe');
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -30,8 +27,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+
+    //3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
 //same shit prettier
