0da98b09ab1411e85fa437d87e7b832d7bc43620
gsikaras <sykarasioannis@hotmail.com>
2024-03-11 17:52:57 +0200

    Addhandler CLicks for paginations complete! lez move on!

--- src/js/controller.js
+++ src/js/controller.js
@@ -55,9 +55,18 @@ const controlSearchResults = async function () {
   }
 };
 
+const controlPagination = function (goToPage) {
+  //1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
 //same shit prettier
